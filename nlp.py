7721eda4a3b2a9352baed4bf452f4ecd57f57888
Berna UZUNOĞLU <bernauzunoglu58@gmail.com>
2024-12-30 22:22:47 +0300

    Text Visualization added.

--- nlp.py
+++ nlp.py
@@ -22,6 +22,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from PIL import Image
+import matplotlib
 import nltk
 from nltk.corpus import stopwords
 from nltk.sentiment import SentimentIntensityAnalyzer
@@ -34,7 +35,7 @@ from wordcloud import WordCloud
 
 filterwarnings('ignore')
 pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 200)
+pd.set_option('display.width', 300)
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
 ##################################################
@@ -83,6 +84,7 @@ df['reviewText'] = df['reviewText'].apply(lambda x: " ".join(x for x in str(x).s
 # nltk.download("punkt")
 # TextBlob(x).words, metni kelimelere ayırarak her bir kelimeyi bir liste elemanı olarak döndüren bir işlemdir. Bu, metin analizinde veya metinden belirli özellikleri çıkarmada yaygın olarak kullanılır.
 df['reviewText'].apply(lambda x: TextBlob(x).words).head()
+print(nltk.data.path)
 
 ###############################
 # Lemmatization - Kelimeleri köklerine indirgeme işlemidir.
@@ -90,3 +92,155 @@ df['reviewText'].apply(lambda x: TextBlob(x).words).head()
 # nltk.download('wordnet')
 df['reviewText'] = df['reviewText'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
 
+##################################################
+# 2. Text Visualization
+##################################################
+###############################
+# Terim Frekanslarının Hesaplanması
+###############################
+# Kelimelerin frekanslarını bulup görselleştirme işlemi yapalım. tf=turn_frekansy
+tf = df['reviewText'].apply(lambda x: pd.value_counts(x.split(" "))).sum(axis=0).reset_index()
+
+tf.columns = ["words", "tf"]
+
+tf.sort_values("tf", ascending=False)
+
+###############################
+# Barplot
+###############################
+matplotlib.use('TkAgg')
+fig, ax = plt.subplots()
+
+# Verilerinizi filtreleyin
+words = tf[tf["tf"] > 500]['words'].tolist()
+tf_values = tf[tf["tf"] > 500]['tf'].tolist()
+
+# Bar rengi ve etiket
+bar_color = 'blue'
+
+# Çubuk grafiği çizin
+ax.bar(words, tf_values, color=bar_color)
+
+# Grafiğin detaylarını ekleyin
+ax.set_ylabel('TF Values')
+ax.set_xlabel('Words')
+ax.set_title('Word Frequency with TF > 500')
+
+# Grafiği göster
+plt.show()
+
+###############################
+# Wordcloud
+###############################
+# Satırları tek bir text dosyası gibi olması için birleştirme yapalım.
+text = " ".join(i for i in df.reviewText)
+
+wordcloud = WordCloud().generate(text)
+plt.imshow(wordcloud,interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+wordcloud = WordCloud(max_font_size=50,
+                      max_words=100,
+                      background_color="white").generate(text)
+
+plt.figure()
+plt.imshow(wordcloud,interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+wordcloud.to_file("wordcloud.png")
+
+###############################
+# Şablonlara Göre Wordcloud
+###############################
+
+tr_mask = np. array(Image.open("tr.png"))
+
+wc = WordCloud(background_color="white",
+               max_words=1000,
+               mask=tr_mask,
+               contour_width=3,
+               contour_color="firebrick")
+
+wc.generate(text)
+plt.figure(figsize=[10,10])
+plt.imshow(wc,interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+##################################################
+# 3. Sentiment Analysis (Duygu Analizi)
+##################################################
+
+
+###############################
+# 4. Feature Engineering
+###############################
+
+
+###############################
+# Count Vectors
+###############################
+
+# Count Vectors: frekans temsiller
+# TF-IDF Vectors: normalize edilmiş frekans temsiller
+# Word Embeddings (Word2Vec, GloVe, BERT vs)
+
+
+# words
+# kelimelerin nümerik temsilleri
+
+# characters
+# karakterlerin numerik temsilleri
+
+# ngram
+
+
+###############################
+# Count Vectors
+###############################
+
+
+# word frekans
+
+
+# n-gram frekans
+
+
+###############################
+# TF-IDF
+###############################
+
+
+###############################
+# 5. Sentiment Modeling
+###############################
+
+# 1. Text Preprocessing
+# 2. Text Visualization
+# 3. Sentiment Analysis
+# 4. Feature Engineering
+# 5. Sentiment Modeling
+
+###############################
+# Logistic Regression
+###############################
+
+
+###############################
+# Random Forests
+###############################
+
+# Count Vectors
+
+
+# TF-IDF Word-Level
+
+
+# TF-IDF N-GRAM
+
+
+###############################
+# Hiperparametre Optimizasyonu
+###############################
